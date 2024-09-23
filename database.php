bd142847f953703ce18f91dd4d730a341d873764
samoha-5475 <48028322+samoha-5475@users.noreply.github.com>
2024-09-23 15:08:37 +0100

    Update database.php
    Removed server details

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 // Defines the parameters for the database connection
-$host = "formula-one-db.czo0uem2g0q9.eu-north-1.rds.amazonaws.com";
-$username = "root";
-$password = "YiWU13=7f?WBlPznBV+";
-$database = "f1db";
+$host = "";
+$username = "";
+$password = "";
+$database = "";
 $port = 3306;
 
 // Creates a connection to a database using the parameters above
